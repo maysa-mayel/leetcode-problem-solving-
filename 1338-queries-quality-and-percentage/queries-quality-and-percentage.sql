eb72522dc0bedbc82d93df613e3e0adfa019dd06
mayssa mayel <114881699+maysa-mayel@users.noreply.github.com>
2025-02-20 00:27:22 +0100

    Time: 780 ms (27.18%) | Memory: 0B (100.00%) - LeetSync

--- 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -4,4 +4,4 @@ select query_name,
 round(sum(rating / position )/count(query_name ),2) as quality, 
 round((sum(rating<3)/ count(rating))*100,2)as poor_query_percentage 
 from Queries 
-group by query_name  ;
+group by query_name;
